900ea376dc19fa2a0eb0f02a0df6892a4da59e0c
David Mohundro <drmohundro@gmail.com>
2009-12-01 13:10:19 -0600

    Removed emacs settings... will need to try it again some other time.

--- .emacs.d/David.el
+++
@@ -1,7 +0,0 @@
-; set up default font
-(set-default-font "-outline-DejaVu Sans Mono-normal-r-normal-normal-16-112-96-96-c-*-iso8859-1")
-
-; color theme
-(require 'color-theme)
-(load-file "~/.emacs.d/elpa-to-submit/blackboard.el")
-(color-theme-blackboard)
