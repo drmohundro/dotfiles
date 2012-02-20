22edf28d3c0d36d1e72d0bea83f34615a4baa216
David Mohundro <david@mohundro.com>
2012-02-20 09:14:36 -0600

    Cleanup, removing some config that isn't used any more.

--- emacs.d/org-mode-setup.el
+++
@@ -1,9 +0,0 @@
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-
-(setq org-log-done 'time)
-(setq org-hide-leading-stars t)
-(setq org-startup-indented t)
-(setq org-M-RET-may-split-line nil)
