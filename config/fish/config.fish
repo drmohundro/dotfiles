f99261227733ea35d033d26d0b322b408bfeba24
David Mohundro <david@mohundro.com>
2015-07-16 13:15:11 -0500

    Initial fish shell support

---
+++ config/fish/config.fish
@@ -0,0 +1,32 @@
+# http://www.martinklepsch.org/posts/git-prompt-for-fish-shell.html
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_char_dirtystate '⚡'
+
+### Prompt
+
+function fish_prompt
+  set_color red
+  echo -n "❯"
+  set_color yellow
+  echo -n "❯"
+  set_color green
+  echo -n "❯ "
+end
+
+function fish_right_prompt
+  set_color blue
+  echo -n (prompt_pwd)
+  set_color green
+  echo -n (__fish_git_prompt)
+  set_color normal
+end
+
+### fasd support
+
+function -e fish_preexec _run_fasd
+  fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
+end
+
+function j
+  cd (fasd -d -e 'printf %s' "$argv")
+end
