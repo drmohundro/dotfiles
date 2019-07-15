fed15c8f5a6b0c9c0c59ed511834a6300d3d8527
David Mohundro <david@mohundro.com>
2019-07-15 13:00:45 -0500

    Add doom emacs config (for orgmode)

---
+++ doom.d/config.el
@@ -0,0 +1,38 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(setq
+ doom-font (font-spec :family "Iosevka" :size 16)
+ doom-theme 'doom-one-light)
+
+; doom-theme 'doom-molokai)
+
+; ln -s /Users/david/Library/Mobile\ Documents/iCloud\~com\~appsonthemove\~beorg/Documents/org org
+(setq org-agenda-files
+      (list
+       "~/org/inbox.org"
+       "~/org/notes.org"
+       "~/org/zignyl.org"))
+
+; log the time when something is marked done
+(setq org-log-done 'time)
+
+(map! (:map override
+        "C-l" #'switch-to-buffer)
+
+      ;; global keybindings
+      :n "J" #'evil-scroll-page-down
+      :n "K" #'evil-scroll-page-up
+
+      :v "\\\\" #'evil-commentary-line)
+
+;      :localleader
+;      "s" #'whitespace-mode)
+
+(setq
+ mac-command-modifier 'meta
+ mac-option-modifier 'super)
+
+(setq doom-localleader-key ",")
+
