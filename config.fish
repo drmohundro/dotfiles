8ec9d31b1c2295d3f28dbd87d72635dc59858a49
David Mohundro <david@mohundro.com>
2021-06-11 08:23:01 -0500

    fix: use -gx for environment vars

--- config.fish
+++ config.fish
@@ -1,23 +1,24 @@
-set -x MANPAGER 'nvim +Man!'
+set -gx MANPAGER 'nvim +Man!'
+set -gx EDITOR nvim
 
 if test -e /usr/local/bin
-  set PATH /usr/local/bin $PATH
+  set -gx PATH /usr/local/bin $PATH
 end
 
 if test -e ~/dev/flutter
-  set PATH $PATH ~/dev/flutter/bin
+  set -gx PATH $PATH ~/dev/flutter/bin
 end
 
 if test -e ~/.dotnet/tools
-  set PATH $PATH ~/.dotnet/tools
+  set -gx PATH $PATH ~/.dotnet/tools
 end
 
 # use gnu versions of coreutils/findutils
 if type -q brew
   # via `brew --prefix FORMULA`... hardcoding for speed, though
-  set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-  set PATH /usr/local/opt/findutils/libexec/gnubin $PATH
-  set PATH /usr/local/opt/grep/libexec/gnubin $PATH
+  set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+  set -gx PATH /usr/local/opt/findutils/libexec/gnubin $PATH
+  set -gx PATH /usr/local/opt/grep/libexec/gnubin $PATH
 end
 
 # local binaries
@@ -25,33 +26,38 @@ set PATH ~/bin $PATH
 
 # tell fzf to use rg to list files
 if type -q fd
-  set -x FZF_DEFAULT_COMMAND 'fd --type f --color=never'
+  set -gx FZF_DEFAULT_COMMAND 'fd --type f --color=never'
 else
-  set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --smart-case --glob "!.git/*"'
+  set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --smart-case --glob "!.git/*"'
 end
 
 # rust global binaries
 if test -e ~/.cargo
-  set PATH $PATH ~/.cargo/bin
+  set -gx PATH $PATH ~/.cargo/bin
+end
+
+# krew for kubectl
+if test -e ~/.krew
+  set -gx PATH $PATH ~/.krew/bin
 end
 
 # go global binaries
 if test -e ~/go
   set -x GOPATH ~/go
   set -x GOROOT "/usr/local/opt/go/libexec"
-  set PATH $PATH $GOPATH/bin
-  set PATH $PATH $GOROOT/bin
+  set -gx PATH $PATH $GOPATH/bin
+  set -gx PATH $PATH $GOROOT/bin
 end
 
 # Python 3 binaries
 if test -e ~/Library/Python/3.9/bin
-  set PATH $PATH ~/Library/Python/3.9/bin
+  set -gx PATH $PATH ~/Library/Python/3.9/bin
 end
 
 # android tooling
 if test -e ~/Library/Android/sdk
-  set ANDROID_SDK ~/Library/Android/sdk
-  set PATH $PATH ~/Library/Android/sdk/platform-tools
+  set -gx ANDROID_SDK ~/Library/Android/sdk
+  set -gx PATH $PATH ~/Library/Android/sdk/platform-tools
 end
 
 # http://www.martinklepsch.org/posts/git-prompt-for-fish-shell.html
@@ -67,7 +73,7 @@ end
 
 # add yarn global binaries (directory comes from `yarn global bin`)
 if begin; type -q yarn; and test -e ~/.config/yarn/global/node_modules/.bin; end
-  set PATH $PATH ~/.config/yarn/global/node_modules/.bin
+  set -gx PATH $PATH ~/.config/yarn/global/node_modules/.bin
 end
 
 # see https://junegunn.kr/2015/03/browsing-git-commits-with-fzf
@@ -85,6 +91,7 @@ end
 alias vim="nvim"
 alias be="bundle exec"
 alias rg="rg --smart-case"
+alias http="xh"
 
 # default to showing the time for the history entries
 function history
