04f3411596a9576c6fac3bf38d97c479b9736b69
David Mohundro <david@mohundro.com>
2021-07-06 20:30:22 -0500

    WIP: more, added telescope config

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,13 +34,24 @@ require 'paq-nvim' {
 g.mapleader = ','
 
 opt.clipboard = 'unnamed'
+opt.hidden = true
+opt.ignorecase = true
+opt.number = true
+opt.smartcase = true
+opt.wildmode = 'longest,list'
 
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.shiftround = true
 opt.expandtab = true
 
+opt.cmdheight = 2
+
 opt.listchars = { tab = '»\t', trail = '·', eol = '↲' }
+opt.modeline = false
+opt.showbreak = '>\\'
+opt.winaltkeys = 'no'
+opt.inccommand = 'split'
 
 opt.completeopt = 'menuone,noselect'
 
@@ -58,11 +69,22 @@ map('x', 'J', '<c-d>')
 map('n', 'K', '<c-u>')
 map('x', 'K', '<c-u>')
 
+-- up/down work as expected with word wrapping on
+map('n', 'j', 'gj')
+map('n', 'k', 'gk')
+map('n', 'gj', 'j')
+map('n', 'gj', 'j')
+
 map('', 'H', '^')
 map('', 'L', '$')
 
 -- clear search highlighting
 map('n', '<esc>', ':nohlsearch<cr><esc>', { silent = true})
+
+map('', '\\\\', '<plug>NERDCommenterInvert', { noremap = false })
+
+map('', '<c-l>', ':Telescope buffers<cr>')
+map('', '<c-p>', ':Telescope find_files<cr>')
 
 opt.termguicolors = true
 
