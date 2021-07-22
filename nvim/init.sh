93ec241dccd31329b7f89f7b7c19c9de8db9b496
David Mohundro <david@mohundro.com>
2021-07-22 17:12:31 -0500

    feat: add init.sh for nvim LSP servers

---
+++ nvim/init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Language servers
+yarn global add bash-language-server
+yarn global add vscode-css-languageserver-bin
+yarn global add vscode-html-languageserver-bin
+yarn global add vscode-json-languageserver
+brew install rust-analyzer
+yarn global add sql-language-server
+yarn global add typescript typescript-language-server
+yarn global add vim-language-server
+yarn global add vls
+yarn global add yaml-language-server
