4195449115670d702b1f1c3e25f0cb311d34f8b3
David Mohundro <david@mohundro.com>
2021-09-14 21:54:02 -0500

    feat: add in nvim-lspinstall

--- nvim/lua/config/lspconfig.lua
+++ nvim/lua/config/lspconfig.lua
@@ -1,7 +1,17 @@
-require('lspconfig').bashls.setup({})
-require('lspconfig').cssls.setup({})
-require('lspconfig').html.setup({})
-require('lspconfig').rust_analyzer.setup({})
-require('lspconfig').tsserver.setup({})
-require('lspconfig').vimls.setup({})
-require('lspconfig').yamlls.setup({})
+-- NOTE: see https://github.com/kabouzeid/nvim-lspinstall#bundled-installers
+
+local function setup_servers()
+  require('lspinstall').setup()
+  local servers = require('lspinstall').installed_servers()
+  for _, server in pairs(servers) do
+    require('lspconfig')[server].setup({})
+  end
+end
+
+setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+require('lspinstall').post_install_hook = function()
+  setup_servers() -- reload installed servers
+  vim.cmd('bufdo e') -- this triggers the FileType autocmd that starts the server
+end
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,8 +16,23 @@ return require('packer').startup(function()
   -- LSP config
   use('neovim/nvim-lspconfig')
 
+  -- LSP install
+  use('kabouzeid/nvim-lspinstall')
+
   -- completion
   use('hrsh7th/nvim-compe')
+
+  use({
+    'folke/trouble.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('trouble').setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  })
 
   -- footer support
   use({
