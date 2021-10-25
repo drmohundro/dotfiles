a0c56a163baf43344d81f99213abbef5cc31d2f6
David Mohundro <david@mohundro.com>
2021-10-25 16:29:42 -0500

    fix: nvim-cmp for tab/s-tab

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -33,9 +33,9 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkey('<C-n>', 'n')
-      elseif vim.fn['vsnip#available']() == 1 then
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn['vsnip#available'](1) == 1 then
         feedkey('<Plug>(vsnip-expand-or-jump)', '')
       elseif has_words_before() then
         cmp.complete()
@@ -48,8 +48,8 @@ cmp.setup({
     }),
 
     ['<S-Tab>'] = cmp.mapping(function()
-      if vim.fn.pumvisible() == 1 then
-        feedkey('<C-p>', 'n')
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif vim.fn['vsnip#jumpable'](-1) == 1 then
         feedkey('<Plug>(vsnip-jump-prev)', '')
       end
