ef0d7b040a9ff21443a3c27506b811224aa222ad
David Mohundro <david@mohundro.com>
2021-11-11 15:19:40 -0600

    feat: add in some more key bindings
    Mostly taken from LunarVim

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -51,17 +51,45 @@ if not g.vscode then
   local wk = require('which-key')
 
   wk.register({
-    f = {
-      '<cmd>Telescope live_grep<cr>',
-      'Live ripgrep search',
+    f = { '<cmd>Telescope live_grep<cr>', 'Live ripgrep search' },
+    r = { '<cmd>Telescope oldfiles<cr>', 'Find recently opened files' },
+
+    d = { '<cmd>NvimTreeToggle<cr>', 'Toggle directory tree' },
+
+    p = {
+      name = 'Packer',
+      c = { '<cmd>PackerCompile<cr>', 'Compile' },
+      i = { '<cmd>PackerInstall<cr>', 'Install' },
+      r = { "<cmd>lua require('lvim.plugin-loader').recompile()<cr>", 'Re-compile' },
+      s = { '<cmd>PackerSync<cr>', 'Sync' },
+      S = { '<cmd>PackerStatus<cr>', 'Status' },
+      u = { '<cmd>PackerUpdate<cr>', 'Update' },
     },
-    r = {
-      '<cmd>Telescope oldfiles<cr>',
-      'Find recently opened files',
-    },
-    d = {
-      '<cmd>NvimTreeToggle<cr>',
-      'Toggle directory tree',
+
+    g = {
+      name = 'Git',
+      j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", 'Next Hunk' },
+      k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", 'Prev Hunk' },
+      l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", 'Blame' },
+      p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", 'Preview Hunk' },
+      r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", 'Reset Hunk' },
+      R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", 'Reset Buffer' },
+      s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", 'Stage Hunk' },
+      u = {
+        "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+        'Undo Stage Hunk',
+      },
+      o = { '<cmd>Telescope git_status<cr>', 'Open changed file' },
+      b = { '<cmd>Telescope git_branches<cr>', 'Checkout branch' },
+      c = { '<cmd>Telescope git_commits<cr>', 'Checkout commit' },
+      C = {
+        '<cmd>Telescope git_bcommits<cr>',
+        'Checkout commit(for current file)',
+      },
+      d = {
+        '<cmd>Gitsigns diffthis HEAD<cr>',
+        'Git Diff',
+      },
     },
   }, {
     prefix = '<leader>',
@@ -71,4 +99,7 @@ if not g.vscode then
 
   map('', '<c-l>', ':Telescope buffers<cr>')
   map('', '<c-p>', ':Telescope find_files<cr>')
+
+  map('n', '<S-l>', ':BufferLineCycleNext<CR>')
+  map('n', '<S-h>', ':BufferLineCyclePrev<CR>')
 end
