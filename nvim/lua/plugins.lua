748451fe8076376b6faafbae762583e37ca3e52a
David Mohundro <david@mohundro.com>
2021-12-06 20:44:24 -0600

    fix: minor toggleterm tweak

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -118,11 +118,10 @@ return require('packer').startup(function()
         -- open_mapping = [[<c-\>]],
         open_mapping = [[<c-t>]],
         hide_numbers = true, -- hide the number column in toggleterm buffers
-        shade_terminals = true,
         start_in_insert = true,
         insert_mappings = true, -- whether or not the open mapping applies in insert mode
         persist_size = false,
-        direction = 'float',
+        direction = 'horizontal',
         close_on_exit = true, -- close the terminal window when the process exits
         shell = 'fish', -- change the default shell
         -- This field is only relevant if direction is set to 'float'
