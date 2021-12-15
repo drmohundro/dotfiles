ce3464c9d0ed4270dcc205ab165c9a7da20ff83e
David Mohundro <david@mohundro.com>
2021-12-15 09:07:13 -0600

    fix: update to latest null-ls config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -245,22 +245,18 @@ return require('packer').startup(function()
     config = function()
       local null_ls = require('null-ls')
 
-      local sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.stylelint,
-        null_ls.builtins.formatting.stylua,
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.rustfmt,
+          null_ls.builtins.formatting.stylelint,
+          null_ls.builtins.formatting.stylua,
 
-        null_ls.builtins.diagnostics.cspell,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.diagnostics.luacheck,
-        null_ls.builtins.diagnostics.proselint,
-      }
-
-      null_ls.config({
-        sources = sources,
-      })
-      require('lspconfig')['null-ls'].setup({
+          null_ls.builtins.diagnostics.cspell,
+          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.diagnostics.luacheck,
+          null_ls.builtins.diagnostics.proselint,
+        },
         on_attach = function(client)
           if client.resolved_capabilities.document_formatting then
             vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
