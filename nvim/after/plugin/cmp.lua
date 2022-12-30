43edd178c2c93295a8e9f6b916b9a4e4d74cff9b
David Mohundro <david@mohundro.com>
2022-12-29 21:26:55 -0600

    fix: completion feels much more natural (to me)

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -2,11 +2,6 @@ if vim.g.vscode then
   return
 end
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
-
 local luasnip = require('luasnip')
 local cmp = require('cmp')
 
@@ -17,7 +12,7 @@ cmp.setup({
       -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` user.
 
       -- For `luasnip` user.
-      -- require('luasnip').lsp_expand(args.body)
+      require('luasnip').lsp_expand(args.body)
 
       -- For `ultisnips` user.
       -- vim.fn["UltiSnips#Anon"](args.body)
@@ -30,27 +25,22 @@ cmp.setup({
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
+    -- see https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#intellij-like-mapping
     ['<Tab>'] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
       if cmp.visible() then
-        cmp.select_next_item()
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          cmp.confirm()
+        end
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
       else
         fallback()
       end
-    end, { 'i', 's' }),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+    end, { 'i', 's', 'c' }),
   },
   sources = {
     { name = 'nvim_lsp' },
