55bb78e825434a98830c8795d732ca4a4aee3d8b
David Mohundro <david@mohundro.com>
2022-12-30 18:59:28 -0600

    fix: warning about notify background

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -11,3 +11,4 @@ vim.cmd([[colorscheme catppuccin]])
 
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NotifyBackground', { bg = 'black' })
