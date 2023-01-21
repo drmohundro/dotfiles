3b423167e00ca9ee37149efa5bd912d809f103d7
David Mohundro <david@mohundro.com>
2023-01-20 21:23:48 -0600

    fix: issue with lsp and typescript

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -72,11 +72,6 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
-  if client.name == 'eslint' then
-    vim.cmd.LspStop('eslint')
-    return
-  end
-
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
   vim.keymap.set('n', '<C-h>', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
