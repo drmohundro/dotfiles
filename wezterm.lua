6bc27c20e67c76bc9b0a2a813e823ffe3dfff1ef
David Mohundro <david@mohundro.com>
2024-08-13 18:58:10 -0500

    feat: initial wezterm tweaks
    Thanks to https://alexplescan.com/posts/2024/08/10/wezterm/ - I haven't
    put projects in yet, but I think I like the idea... not sure if I'd use
    it, though, because I use zoxide so much.

--- wezterm.lua
+++ wezterm.lua
@@ -1,27 +1,38 @@
 -- Pull in the wezterm API
 local wezterm = require('wezterm')
 
--- This table will hold the configuration.
-local config = {}
+local config = wezterm.config_builder()
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-  config = wezterm.config_builder()
+-- NOTE: do we want to pull in the appearance library or not?
+local use_dark = true
+
+if use_dark then
+  config.color_scheme = 'tokyonight_night'
+else
+  config.color_scheme = 'tokyonight_day'
 end
 
--- This is where you actually apply your config choices
+-- Slightly transparent and blurred background
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 30
+-- Removes the title bar, leaving only the tab bar. Keeps
+-- the ability to resize by dragging the window's edges.
+-- On macOS, 'RESIZE|INTEGRATED_BUTTONS' also looks nice if
+-- you want to keep the window controls visible and integrate
+-- them into the tab bar.
+config.window_decorations = 'RESIZE'
 
--- For example, changing the color scheme:
-config.color_scheme = 'tokyonight_night'
+-- Sets the font for the window frame (tab bar)
+config.window_frame = {
+  -- Berkeley Mono for me again, though an idea could be to try a
+  -- serif font here instead of monospace for a nicer look?
+  font = wezterm.font({ family = 'JetBrainsMono Nerd Font', weight = 'Bold' }),
+  font_size = 13,
+}
 
--- Light (Presentations)
--- config.color_scheme = 'PaperColor Light (base16)'
-
-config.font_size = 17
+config.font_size = 15
 config.font = wezterm.font_with_fallback({
-  'Iosevka',
-  { family = 'Victor Mono', weight = 'Medium' },
+  -- 'Iosevka', (for some crazy reason, it is super slow)
   'JetBrainsMono Nerd Font',
 })
 
@@ -31,7 +42,68 @@ else
   config.default_prog = { '/usr/local/bin/fish', '-l' }
 end
 
-config.hide_tab_bar_if_only_one_tab = true
+local function segments_for_right_status(window)
+  return {
+    window:active_workspace(),
+    wezterm.strftime('%a %b %-d %-I:%M %p'),
+    wezterm.hostname(),
+  }
+end
+
+wezterm.on('update-status', function(window, _)
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  local segments = segments_for_right_status(window)
+
+  local color_scheme = window:effective_config().resolved_palette
+  -- Note the use of wezterm.color.parse here, this returns
+  -- a Color object, which comes with functionality for lightening
+  -- or darkening the colour (amongst other things).
+  local bg = wezterm.color.parse(color_scheme.background)
+  local fg = color_scheme.foreground
+
+  -- Each powerline segment is going to be coloured progressively
+  -- darker/lighter depending on whether we're on a dark/light colour
+  -- scheme. Let's establish the "from" and "to" bounds of our gradient.
+  local gradient_to, gradient_from = bg
+
+  -- if appearance.is_dark() then
+  if use_dark then
+    gradient_from = gradient_to:lighten(0.2)
+  else
+    gradient_from = gradient_to:darken(0.2)
+  end
+
+  -- Yes, WezTerm supports creating gradients, because why not?! Although
+  -- they'd usually be used for setting high fidelity gradients on your terminal's
+  -- background, we'll use them here to give us a sample of the powerline segment
+  -- colours we need.
+  local gradient = wezterm.color.gradient(
+    {
+      orientation = 'Horizontal',
+      colors = { gradient_from, gradient_to },
+    },
+    #segments -- only gives us as many colours as we have segments.
+  )
+
+  -- We'll build up the elements to send to wezterm.format in this table.
+  local elements = {}
+
+  for i, seg in ipairs(segments) do
+    local is_first = i == 1
+
+    if is_first then
+      table.insert(elements, { Background = { Color = 'none' } })
+    end
+    table.insert(elements, { Foreground = { Color = gradient[i] } })
+    table.insert(elements, { Text = SOLID_LEFT_ARROW })
+
+    table.insert(elements, { Foreground = { Color = fg } })
+    table.insert(elements, { Background = { Color = gradient[i] } })
+    table.insert(elements, { Text = ' ' .. seg .. ' ' })
+  end
+
+  window:set_right_status(wezterm.format(elements))
+end)
 
 config.keys = {
   {
@@ -61,5 +133,4 @@ config.keys = {
   },
 }
 
--- and finally, return the configuration to wezterm
 return config
