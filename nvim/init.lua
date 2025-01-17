3a313b38551c06962ab3b05f638013f8117c2705
David Mohundro <david@mohundro.com>
2025-01-16 21:23:56 -0600

    feat: switch to lazyvim
    I'm sort of tired of chasing changing plugins and would rather just get general config into a common set up... I still enjoy playing with new configs, but this set up is so much nicer (and even faster) than what I had before with far less effort.
    
    The main thing I'm missing is _my_ keyboard shortcuts. I'm not sure if I'm missing any needed plugins yet or not, but we'll see.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,2 @@
-require('drmohundro')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "echasnovski/mini.comment",
+    opts = {
+      mappings = {
+        comment = "\\\\",
+        comment_line = "\\\\",
+        comment_visual = "\\\\",
+      },
+    },
+  },
+
+  {
+    "NeogitOrg/neogit",
+    event = "VeryLazy",
+  },
+}
